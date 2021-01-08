63ca31ace632f3b890104dc19f92296f0b8080fb
Kris <63805048+ravence@users.noreply.github.com>
2021-01-08 16:42:31 -0500

    Switched to MVC internally

--- src/index.js
+++ src/index.js
@@ -1,29 +1,86 @@
-import './style.scss'; 
+import './style.scss';
 
-$.getJSON({
-  url: "https://api.github.com/repos/thomasdavis/backbonetutorials/contributors",
-  success: function(data) {
+class Model {
+    constructor(sourceURL) {
+        this.url = sourceURL;
+        this.contacts = [];
+    }
 
-      let htmlStr = "<tbody>";
+    loadData() {
+        var ths = this;
 
-      for (let i=0; i < data.length; i++) {
-          htmlStr += ("<tr>" +
-          "<td><img src='" + data[i].avatar_url + "'></img></td>" +
-          "<td>" + data[i].id + "</td>" +
-          "<td>" + data[i].login + "</td>" +
-          "<td>" + data[i].url + "</td>" +
-          "<td>" + data[i].contributions + "</td>" +
-          "</tr>");
+        $.getJSON({
+            url: this.url,
+            success: function(data) {
+                ths.contacts = data;
+                ths.onChangeContacts(ths.contacts);
+            }
+        })
+    }
 
-      }
+    editContact(index, field, data) {
+        if (this.contats[index].hasOwnProperty(field))
+            this.contacts[index][field] = data;
 
-      htmlStr += "</tbody>"
+        this.onChangeContacts(this.contacts);
+    }
 
-      $("table").append(htmlStr);
-  },
-  fail: function() {
-      alert("Something went wrong!");
-  }
-});
+    bindOnChangeContacts(fcn) {
+        this.onChangeContacts = fcn;
+    }
+}
 
-//
+class View {
+    constructor() {
+        $("main").append("<table></table>");
+        $("main table").append("<thead></thead>");
+
+        $("main table thead").append("<tr></tr>");
+
+        $("main table thead tr").append("<th></th>");
+        $("main table thead tr").append("<th>id</th>");
+        $("main table thead tr").append("<th>username</th>");
+        $("main table thead tr").append("<th>contributions</th>");
+    }
+
+    displayContacts(contacts) {
+        $("table").not("thead").remove("tr");
+
+        let htmlStr = "<tbody>";
+        contacts.forEach((contact) => {
+            htmlStr += "<tr>";
+
+            htmlStr += "<td><img src='" + contact.avatar_url + "' /></td>";
+
+            htmlStr += "<td>" + contact.id + "</td>" +
+                "<td>" + contact.login + "</td>" +
+                "<td>" + contact.contributions + "</td>"
+            htmlStr += "</tr>";
+        })
+        htmlStr += "</tbody>";
+
+        $("main table").append(htmlStr);
+    }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        this.model.bindOnChangeContacts(this.view.displayContacts);
+        this.model.loadData();
+    }
+}
+
+new Controller(new Model("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors"),
+    new View());
+
+// CONTACTS:
+// stores the array of js objects
+
+// DISPLAY:
+// takes js objects and puts in table
+
+// START:
+// changes data and then updates display
